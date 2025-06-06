9ca0b6ba7bd2c197b29a06d207efb366badfc9d0
Franaffel <151704994+FraN-onLine@users.noreply.github.com>
2025-06-06 21:53:01 +0800

    Add files via upload
    Player Script + Enemy Script

--- gamejamlbentry/Character/player.gd
+++ gamejamlbentry/Character/player.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends CharacterBody2D
 
 class_name Player
 
@@ -7,8 +7,8 @@ signal healthChanged
 @onready var healthbar: ProgressBar = $Healthbar
 @export var speed: float = 200.0
 @export var camera: Camera2D
-var max_health = 200
-var health = 200
+var max_health = 100
+var health = 100
 
 func _ready():
 	health = max_health
@@ -19,11 +19,15 @@ func _process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	if input_vector.length() > 0:
-		input_vector = input_vector.normalized()
-		position += input_vector * speed * delta
+		input_vector = input_vector.normalized() * speed
+	else:
+		input_vector = Vector2.ZERO
 
-		if input_vector.x != 0:
-			$AnimatedSprite2D.flip_h = input_vector.x < 0
+	velocity = input_vector
+	move_and_slide()
+
+	if input_vector.x != 0:
+		$AnimatedSprite2D.flip_h = input_vector.x < 0
 	
 	var arrow = $Hand
 	if arrow:
@@ -36,16 +40,13 @@ func _process(delta):
 	if camera:
 		camera.position = position
 
-func take_damage(damage, attack_type):
+func take_damage(damage):
 	health -= damage
 	healthbar._set_health(health)
 	emit_signal("healthChanged", health)
 	if health <= 0:
 		health = 0
-		#$explosion/CPUParticles2D.emitting = true
-		$Sprite2D/AnimationPlayer.play("idle")
-		$Sprite2D/AnimationPlayer.stop()
-		$Sprite2D.visible = false
+		$AnimatedSprite2D.visible = false
 		healthbar.visible = false
 		print("Unit is dead!")
 		self.queue_free()
