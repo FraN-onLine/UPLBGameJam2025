338bffc2c2dad3bfff527f5ac1caf9754e3d73c6
Andres John Abad Ruadap <ajgwaps07@gmail.com>
2025-06-06 21:30:18 +0800

    added map

--- gamejamlbentry/Character/fixed_player.gd
+++ gamejamlbentry/Character/fixed_player.gd
@@ -1,23 +1,23 @@
-extends Sprite2D
+extends CharacterBody2D
 
 class_name Player
 
+signal healthChanged
+
+@onready var healthbar: ProgressBar = $Healthbar
 @export var speed: float = 200.0
 @export var camera: Camera2D
-var HP = 200
+var max_health = 200
+var health = 200
 
 func _process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	if input_vector.length() > 0:
-		input_vector = input_vector.normalized()
-		position += input_vector * speed * delta
+		input_vector = input_vector.normalized() * speed
+	else:
+		input_vector = Vector2.ZERO
 
-		# Sprite flipping logic for AnimatedSprite2D
-		if input_vector.x != 0:
-			$AnimatedSprite2D.flip_h = input_vector.x < 0
-
-	# Camera following logic
-	if camera:
-		camera.position = position
+	velocity = input_vector
+	move_and_slide()
